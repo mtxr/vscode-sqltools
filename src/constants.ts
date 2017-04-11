5bf08b01609453c85fb909a9d01fb5274b196017
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-04-11 03:17:04 -0300

    Fix version

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import { Utils } from './api';
 
 export default class Constants {
-  public static version      = 'v0.0.2';
+  public static version      = 'v0.1.1';
   public static extNamespace = 'SQLTools';
   public static outputChannelName = Constants.extNamespace;
   public static bufferName   = path.join(Utils.getHome(), 'SQLTools.buffer.sql');
