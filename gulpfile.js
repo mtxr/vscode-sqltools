cc945cbbff0e94fbc2bf92e42a3bc94cc35640b8
Matheus Teixeira <matheus.mtxr@gmail.com>
2017-04-15 12:40:56 -0300

    disabled coverage for gulp for now

--- gulpfile.js
+++ gulpfile.js
@@ -25,24 +25,24 @@ gulp.task('compile', () => {
     .pipe(gulp.dest('out'))
 })
 
-gulp.task('pre-test', ['compile'], () => {
-  // This tells gulp which files you want to pipe
-  // In our case we want to pipe every `.js` file inside any folders inside `test`
-  return gulp.src('./out/**/*.js')
-    .pipe(istanbul({includeUntested: true}))
-    // This overwrites `require` so it returns covered files
-    .pipe(istanbul.hookRequire())
-})
+// gulp.task('pre-test', ['compile'], () => {
+//   // This tells gulp which files you want to pipe
+//   // In our case we want to pipe every `.js` file inside any folders inside `test`
+//   return gulp.src('./out/**/*.js')
+//     .pipe(istanbul({includeUntested: true}))
+//     // This overwrites `require` so it returns covered files
+//     .pipe(istanbul.hookRequire())
+// })
 
 gulp.task('test', ['compile'], () => {
   runTests()
 })
 
-gulp.task('coverage', ['pre-test'], () => {
-  runTests()
-    // Here we will create report files using the test's results
-    .pipe(istanbul.writeReports())
-})
+// gulp.task('coverage', ['pre-test'], () => {
+//   runTests()
+//     // Here we will create report files using the test's results
+//     .pipe(istanbul.writeReports())
+// })
 
 gulp.task('watch', () => {
   gulp.watch('./src/**/*.ts', ['compile'])
