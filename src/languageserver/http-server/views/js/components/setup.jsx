719e8001c921017623e27f71bf17892da534bda2
Mickey Pearce <mickey.pearce@mail.com>
2018-05-16 12:40:24 +0200

    Add OracleDB support

--- src/languageserver/http-server/views/js/components/setup.jsx
+++ src/languageserver/http-server/views/js/components/setup.jsx
@@ -12,7 +12,8 @@ import Loading from './loading.jsx'
 const dialectDefaultPorts = {
   'MySQL': 3306,
   'MSSQL': 1433,
-  'PostgreSQL': 5432
+  'PostgreSQL': 5432,
+  'OracleDB': 1521
 }
 
 class Syntax extends React.Component {
