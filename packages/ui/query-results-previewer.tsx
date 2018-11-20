2ff2d32c273f9077d95f3d07fac89b4904689793
Matheus Teixeira <matheus.mtxr@gmail.com>
2018-11-19 23:06:56 -0200

    Ui files reorganized

---
+++ packages/ui/query-results-previewer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/query-results';
+import './sass/app.scss';
+
+render(<App />, document.getElementById('root'));
---
+++ packages/ui/settings-editor.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/setup';
+import './sass/app.scss';
+
+render(<App />, document.getElementById('root'));
