12e52491be626996abd9c50a20b6c4f6c4f4e8c8
Matheus Teixeira <matheus.mtxr@gmail.com>
2019-04-03 23:46:15 -0300

    master: Test runner with coverage

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ...(require('./test/config/baseConfig')),
+  preset: 'ts-jest',
+  collectCoverageFrom: ['**/*.ts', '**/*.tsx'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageThreshold: {
+    global: {
+      statements: 13,
+      branches: 7,
+      functions: 16,
+      lines: 14,
+    },
+  },
+  testMatch: ['**/*.test.(ts)'],
+};
