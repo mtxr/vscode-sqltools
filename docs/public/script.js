05621433a9ca4223c43cc43f8f092222a340ee04
Matheus Teixeira <matheus.mtxr@gmail.com>
2020-02-24 16:50:13 -0300

    docs-test docs

--- docs/public/script.js
+++ docs/public/script.js
@@ -1,65 +1,67 @@
-window.__codefundDisabled = true;
-window.__carbonDisabled = false;
-var prevHref = '';
-var id = 'codefund';
-var index = 0;
-
-function loadScript(src, id, position) {
-  var script = document.createElement('script');
-  script.setAttribute('async', '');
-  script.type = 'text/javascript';
-  script.src = src;
-  script.id = id;
-  position.appendChild(script);
-
-  return script;
-}
-
-function adCodeFund(props) {
-  if (window.__codefundDisabled) return;
-  try {
-    var el = document.getElementById(id);
-    if (!el) return;
-    id = 'codefund-' + index;
-    el.id = id;
-    var srcript = document.getElementById('codefund-src');
-    srcript.remove();
-    loadScript('https://app.codefund.io/properties/684/funder.js?target=codefund-' + index, 'codefund-src', document.body);
-    index++;
-  }
-  catch (error) {
-    console.log(error);
-  }
-};
-
-function adCarbon(props) {
-  if (window.__carbonDisabled) return;
-  try {
-    var el = document.getElementById('carbon-ad');
-    if (!el) return;
-    loadScript('//cdn.carbonads.com/carbon.js?serve=CE7ITK3L&placement=vscode-sqltoolsmteixeiradev', '_carbonads_js', el);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function checkLocation(props) {
-  if (prevHref && props.location.href === prevHref) return;
-  setTimeout(() => {
-    prevHref = props.location.href;
-    adCarbon(props);
-    adCodeFund(props);
-  }, 500);
-}
+window.__codefundEnabled = true;
+window.__carbonEnabled = true;
 
 (function(w) {
-  w.addEventListener('pagechanged', function(
+  var prevHref = '';
+  var codefundId = 'codefund';
+  var codefundIndex = 0;
+  function loadScript(src, id, position) {
+    var script = document.createElement('script');
+    script.setAttribute('async', '');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.id = id;
+    position.appendChild(script);
+  
+    return script;
+  }
+  
+  function adCodeFund(props) {
+    if (!window.__codefundEnabled) return;
+    try {
+      var el = document.getElementById(codefundId);
+      if (el) el.remove();
+      var script = document.getElementById('codefund-src');
+      if (script) script.remove();
+  
+      codefundId = 'codefund-' + codefundIndex;
+      el = document.createElement('div');
+      el.id = codefundId;
+      document.body.appendChild(el);
+      loadScript('https://app.codefund.io/properties/684/funder.js?target=codefund-' + codefundIndex, 'codefund-src', document.body);
+      codefundIndex++;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+  
+  function adCarbon(props) {
+    if (!window.__carbonEnabled) return;
+    try {
+      var el = document.getElementById('carbon-ad');
+      if (!el) return;
+      loadScript('//cdn.carbonads.com/carbon.js?serve=CE7ITK3L&placement=vscode-sqltoolsmteixeiradev', '_carbonads_js', el);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  function checkLocation(props) {
+    if (prevHref && props.location.href === prevHref) return;
+    setTimeout(() => {
+      prevHref = props.location.href;
+      adCarbon(props);
+      adCodeFund(props);
+    }, 500);
+  }
+  window.addEventListener('pagechanged', function(
     /** @type {CustomEvent} */
     event
   ) {
     checkLocation(event.detail)
   });
-  w.addEventListener('docs-loaded', function(event) {
+  window.addEventListener('docs-loaded', function(event) {
     var prevOnLoad = window.onload;
     window.onload = function() {
       prevOnLoad && prevOnLoad.apply(window, arguments);
@@ -74,4 +76,4 @@ function checkLocation(props) {
       }, 1000);
     }
   })
-})(window);
+})();
