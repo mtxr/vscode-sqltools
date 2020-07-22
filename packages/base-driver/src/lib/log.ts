67d533319d5f904766a57f847773ad70cb474c7a
Matheus <matheus.mtxr@gmail.com>
2020-07-21 22:52:59 -0300

    temporary solution for #644. Closes #644

--- packages/base-driver/src/lib/log.ts
+++ packages/base-driver/src/lib/log.ts
@@ -2,7 +2,7 @@ process.env.DEBUG_HIDE_DATE = '1';
 
 import debug from 'debug';
 
-debug.enable(process.env.NODE_ENV === 'development' ? '*,-babel*' : '*,-babel*,-*:debug,-*:*:debug,-*:*:*:debug,-*:*:*:*:debug,-*:*:*:*:*:debug');
+debug.enable(process.env.NODE_ENV === 'development' ? 'sql:*' : 'sql:*,-sql:*:debug,-sql:*:*:debug,-sql:*:*:*:debug,-sql:*:*:*:*:debug,-sql:*:*:*:*:*:debug');
 
 (debug as any).formatArgs = function(args: any) {
   args[0] = `['${this.namespace}'] ${args[0]}`;
