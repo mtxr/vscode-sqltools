0314e696fc8ed27adc862bf347da8472a9689fd1
Matheus <matheus.mtxr@gmail.com>
2020-07-21 23:59:05 -0300

    fix blank screen issue. closes #639

--- packages/vscode/webview-provider/index.ts
+++ packages/vscode/webview-provider/index.ts
@@ -105,7 +105,7 @@ export default abstract class WebviewProvider<State = any> implements Disposable
 
   public prepareUrl(localResource: Uri | string) {
     return this.panel && this.panel.webview
-      ? this.panel.webview.asWebviewUri(Uri.parse(localResource.toString()))
+      ? this.panel.webview.asWebviewUri(Uri.file(localResource.toString()))
       : null;
   }
   public hide = () => {
