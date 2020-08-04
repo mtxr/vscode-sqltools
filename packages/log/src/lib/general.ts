89366b21d5ca9d06af5411599f5a023e96c90ae3
Matheus <matheus.mtxr@gmail.com>
2020-08-04 01:48:22 -0300

    log utility

---
+++ packages/log/src/lib/general.ts
@@ -0,0 +1,3 @@
+import factory from './factory';
+
+export default factory();
---
+++ packages/log/src/lib/vscode.ts
@@ -0,0 +1,26 @@
+if (process.env.PRODUCT !== 'ext') { throw 'Cant use outputchannels outside of VSCode context'; }
+
+import { Writable }  from 'stream';
+import { window }  from 'vscode';
+import factory from './factory';
+
+const outputChannel = window.createOutputChannel(process.env.DISPLAY_NAME || 'SQLTools');
+
+const writableStream = new Writable({
+  write: (chunk, _, done) => {
+    outputChannel.append(chunk.toString(chunk.encoding || 'utf8'));
+    done();
+  },
+  writev: (chunks, done) => {
+    chunks.forEach(i => outputChannel.append(i.chunk.toString(i.encoding || 'utf8')));
+    done();
+  },
+});
+
+const logger = factory({}, writableStream);
+
+logger.clear = outputChannel.clear.bind(outputChannel);
+logger.outputChannel = outputChannel;
+logger.show = () => outputChannel.show();
+
+export default logger;
