508f0a4a7637b64eb9ccfa5468f5e5e9e4af27d0
ultram4rine <ultramarine730@gmail.com>
2020-10-30 14:12:14 +0400

    register cockroach

--- packages/driver.pg/src/constants.ts
+++ packages/driver.pg/src/constants.ts
@@ -3,4 +3,5 @@ import { IDriverAlias } from '@sqltools/types';
 export const DRIVER_ALIASES: IDriverAlias[] = [
   { displayName: 'PostgreSQL', value: 'PostgreSQL'},
   { displayName: 'AWS Redshift', value: 'AWS Redshift'},
+  { displayName: 'CockroachDB', value: 'CockroachDB'},
 ];
