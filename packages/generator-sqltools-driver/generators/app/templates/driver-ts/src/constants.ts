b0e261bc8d19d6285db43b52858935eb42f0ba82
Matheus Teixeira <matheus.mtxr@gmail.com>
2021-03-20 00:28:47 -0300

    deprecate yeoman generator in favor of github template repositories

--- packages/generator-sqltools-driver/generators/app/templates/driver-ts/src/constants.ts
+++
@@ -1,8 +0,0 @@
-import { IDriverAlias } from '@sqltools/types';
-
-/**
- * Aliases for yout driver. EG: PostgreSQL, PG, postgres can all resolve to your driver
- */
-export const DRIVER_ALIASES: IDriverAlias[] = [
-  { displayName: '<%= displayName %>', value: '<%= displayName %>'},
-];
