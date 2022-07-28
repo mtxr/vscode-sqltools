c56c7d1e0b0f548f312bd7eb1d89d7a39868b9c6
John Murray <johnm@georgejames.com>
2022-07-28 18:18:34 +0000

    Use sudo when removing previous local-mysqld dir

--- test/docker/mysql/prepare.sh
+++ test/docker/mysql/prepare.sh
@@ -4,6 +4,6 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rm $SCRIPT_DIR/local-mysqld -rf
+sudo rm $SCRIPT_DIR/local-mysqld -rf
 mkdir -p $SCRIPT_DIR/local-mysqld
 chmod -R 777 $SCRIPT_DIR/local-mysqld
