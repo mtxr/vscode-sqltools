4931ce6573e42392dfc7202cecd923f6b6a7f7a3
Matheus Teixeira <me@mteixeira.dev>
2022-08-21 03:42:32 -0300

    [dev] auto update readme for extension when building

--- build-tools/webpack.config.js
+++ build-tools/webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const fs = require('fs');
 
 require('dotenv').config({
-  path: path.resolve(__dirname, '..', '.env')
+  path: path.resolve(__dirname, '..', '.env'),
 });
 
 const constants = require('./constants');
 const { author, version, packagesDir, outdir } = constants;
 
 // const copyEntries = [];
-module.exports = function(env = {}) {
+module.exports = function (env = {}) {
   env.pkg = env.pkg || [];
   if (typeof env.pkg === 'string') {
     env.pkg = [env.pkg];
@@ -25,9 +25,9 @@ module.exports = function(env = {}) {
     IS_PRODUCTION: ${constants.IS_PRODUCTION}
 
   #################################
-  `)
+  `);
 
-  fs.readdirSync(packagesDir).forEach((pkg) => {
+  fs.readdirSync(packagesDir).forEach(pkg => {
     if (env.pkg.length > 0 && env.pkg.indexOf(pkg) === -1) {
       return console.log(`Skipping @sqltools/${pkg}`);
     }
@@ -38,23 +38,40 @@ module.exports = function(env = {}) {
     console.log(`Reading package @sqltools/${pkg} config`);
     const buildJson = require(`${pkgPath}/build.json`);
 
+    if (buildJson.patch) {
+      buildJson.patch.forEach(patch => {
+        const transformer = transformers[patch.type];
+        transformer(pkgPath, patch);
+      });
+    }
+
     if (buildJson.build) {
-      console.log(`\t>> Found ${buildJson.build.length} build entries`)
-      buildJson.build.forEach(({
-        entries,
-        type,
-        externals = {},
-      }) => {
-        webpackConfigs.push(require(`./webpack/${type}.config.js`)({
-          entries,
-          packagePath: pkgPath,
-          externals,
-        }));
-      })
+      console.log(`\t>> Found ${buildJson.build.length} build entries`);
+      buildJson.build.forEach(({ entries, type, externals = {} }) => {
+        webpackConfigs.push(
+          require(`./webpack/${type}.config.js`)({
+            entries,
+            packagePath: pkgPath,
+            externals,
+          })
+        );
+      });
     } else {
-      console.log(`\t>> no build entries`)
+      console.log(`\t>> no build entries`);
     }
   });
 
   return webpackConfigs;
-}
+};
+
+const transformers = {
+  contentReplace: (pkgPath, { entries, regexStr, replacement }) => {
+    const regex = new RegExp(regexStr, 'mi');
+
+    Object.entries(entries).forEach(([outfileName, sourceFileName]) => {
+      let content = fs.readFileSync(path.resolve(pkgPath, sourceFileName)).toString();
+      content = content.replace(regex, replacement);
+      fs.writeFileSync(path.resolve(pkgPath, outfileName), content);
+    });
+  },
+};
