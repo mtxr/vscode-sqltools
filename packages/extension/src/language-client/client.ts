mtxr/vscode-sqltools

commit c22b4e5cb3726b1f2189fc65718884e6b33a95a2

Enhance node path detection. Make node runtime default if available (#957)
* [infinity-loop-on-auto-install-fails] avoid infinity loop if auto install fails

* [auto-detect-node-path] detect node automatically in the machine and prefer node runtime if available

* Try to handle PowerShell quirk

* Tweak install messages

* Updated yarn.lock

* Update packages/extension/src/language-client/client.ts

Co-authored-by: John Murray <johnm@georgejames.com>

* [auto-detect-node-path] fixes exit command on other places

* [auto-detect-node-path] make VS Code name consistent across the codebase

Co-authored-by: gjsjohnmurray <johnm@georgejames.com>

packages/extension/src/language-client/client.ts
@@ -13,6 +13,7 @@ import Context from '@sqltools/vscode/context';
 import uniq from 'lodash/uniq';
 import { ElectronNotSupportedNotification } from '@sqltools/base-driver/dist/lib/notification';
 import { ExitCalledNotification, ServerErrorNotification } from '@sqltools/language-server/src/notifications';
+import detectNodePath from "./detect-node-path";
 
 const log = createLogger('lc');
 
@@ -22,24 +23,6 @@ export class SQLToolsLanguageClient implements ILanguageClient {
 
   private avoidRestart = false;
   constructor() {
-    this.client = new LanguageClient(
-      EXT_CONFIG_NAMESPACE,
-      `${DISPLAY_NAME} Language Server`,
-      this.getServerOptions(),
-      this.getClientOptions(),
-      );
-    const defaultErrorHandler = this.client.createDefaultErrorHandler();
-
-    this.clientErrorHandler = {
-      error: defaultErrorHandler.error,
-      closed: (): CloseAction => {
-        if (this.avoidRestart) {
-          return CloseAction.DoNotRestart;
-        }
-        return defaultErrorHandler.closed();
-      },
-    };
-
     this.onNotification(ExitCalledNotification, () => {
       this.avoidRestart = true;
     });
@@ -61,6 +44,27 @@ export class SQLToolsLanguageClient implements ILanguageClient {
     });
   }
 
+  public async init() {
+    const serverOptions = await this.getServerOptions();
+    this.client = new LanguageClient(
+      EXT_CONFIG_NAMESPACE,
+      `${DISPLAY_NAME} Language Server`,
+      serverOptions,
+      this.getClientOptions(),
+    );
+    const defaultErrorHandler = this.client.createDefaultErrorHandler();
+
+    this.clientErrorHandler = {
+      error: defaultErrorHandler.error,
+      closed: (): CloseAction => {
+        if (this.avoidRestart) {
+          return CloseAction.DoNotRestart;
+        }
+        return defaultErrorHandler.closed();
+      },
+    };
+
+  }
   public start() {
     return this.client.start();
   }
@@ -84,16 +88,22 @@ export class SQLToolsLanguageClient implements ILanguageClient {
     return this.client.onNotification.apply(this.client, arguments);
   }
 
-  private getServerOptions(): ServerOptions {
+  private async getServerOptions(): Promise<ServerOptions> {
     const serverModule = Context.asAbsolutePath('dist/languageserver.js');
+    const nodePath = await detectNodePath();
     let runtime: string = undefined;
     const useNodeRuntime = Config.useNodeRuntime;
+
     if (useNodeRuntime) {
       if (typeof useNodeRuntime === 'string') {
         const runtimePath = path.normalize(useNodeRuntime);
         if (fs.existsSync(runtimePath)) {
           runtime = runtimePath;
         }
+      } else if (nodePath) {
+        const message = `Node runtime auto detected.\nUsing ${nodePath}.`;
+        window.showInformationMessage(message);
+        runtime = nodePath
       } else {
         if (commandExists('node')) {
           runtime = 'node';
@@ -142,7 +152,7 @@ export class SQLToolsLanguageClient implements ILanguageClient {
       },
     };
     let selector = [];
-    if (Config.completionLanguages){
+    if (Config.completionLanguages) {
       selector = selector.concat(Config.completionLanguages);
     }
 
@@ -213,15 +223,15 @@ export class SQLToolsLanguageClient implements ILanguageClient {
 
   private electronNotSupported = async () => {
     const r = await window.showInformationMessage(
-      `VSCode engine is not supported. You should enable \'${EXT_CONFIG_NAMESPACE}.useNodeRuntime\' and have NodeJS installed to continue.`,
+      `VS Code engine is not supported. You should enable \'${EXT_CONFIG_NAMESPACE}.useNodeRuntime\' and have NodeJS installed to continue.`,
       'Enable now',
     );
     if (!r) return;
     await Wspc.getConfiguration(EXT_CONFIG_NAMESPACE).update('useNodeRuntime', true, ConfigurationTarget.Global);
-    try { await Wspc.getConfiguration(EXT_CONFIG_NAMESPACE).update('useNodeRuntime', true, ConfigurationTarget.Workspace) } catch(e) {}
-    try { await Wspc.getConfiguration(EXT_CONFIG_NAMESPACE).update('useNodeRuntime', true, ConfigurationTarget.WorkspaceFolder) } catch(e) {}
+    try { await Wspc.getConfiguration(EXT_CONFIG_NAMESPACE).update('useNodeRuntime', true, ConfigurationTarget.Workspace) } catch (e) { }
+    try { await Wspc.getConfiguration(EXT_CONFIG_NAMESPACE).update('useNodeRuntime', true, ConfigurationTarget.WorkspaceFolder) } catch (e) { }
     const res = await window.showInformationMessage(
-      `\'${EXT_NAMESPACE}.useNodeRuntime\' enabled. You must reload VSCode to take effect.`, 'Reload now');
+      `\'${EXT_NAMESPACE}.useNodeRuntime\' enabled. You must reload VS Code to take effect.`, 'Reload now');
     if (!res) return;
     commands.executeCommand('workbench.action.reloadWindow');
   }

packages/extension/src/language-client/detect-node-path.ts
@@ -1,0 +1,22 @@
+import { getDataPath } from "@sqltools/util/path";
+import { window } from 'vscode';
+import fs from "fs";
+import getShellExitCommand from "@sqltools/vscode/utils/get-shell-exit-cmd";
+
+const nodeRuntimeTmpFile = getDataPath(".node-runtime");
+
+
+const detectNodePath = async (): Promise<string | null> => {
+  try {
+    const terminal = window.createTerminal({ name: "detect node runtime" });
+    await new Promise<void>((resolve) => {
+      window.onDidCloseTerminal((e => e.processId === terminal.processId && resolve()))
+      terminal.sendText(`node -e 'require("fs").writeFileSync("${nodeRuntimeTmpFile}", process.execPath)'; ${getShellExitCommand()}`);
+    })
+    return fs.readFileSync(nodeRuntimeTmpFile).toString();
+  } catch (error) {
+    return null
+  }
+}
+
+export default detectNodePath;

packages/vscode/utils/get-shell-exit-cmd.ts
@@ -1,0 +1,7 @@
+import { env } from 'vscode';
+
+export default function getShellExitCommand(code = 0) {
+  const isPowerShell = env.shell.match(/[\\/]pwsh(\.exe)?$/g);
+  if (isPowerShell) return `$(exit ${code})`;
+  return `exit ${code}`;
+}

dev.todo
@@ -1,17 +1,0 @@
-TODO:
-  DRIVERS:
-    - build all script
-  EXPLORER:
-    - Allow to add to auto connect using explorer
-    - Allow to open Bookmarks file
-  RESULTS:
-    - Add option to refresh query results with hotkey or button
-
-VNEXT:
-  DOCS:
-    - write docs about settings
-    - SSL Certs PGSQL
-    - PGSQL: auto connect on connection lost
-    - add docs for formatting
-  DEP MANAGER:
-    - Allow to use VSCode terminal api

packages/extension/src/index.ts
@@ -50,6 +50,7 @@ export class SQLToolsExtension implements IExtension {
     this.onWillRunCommandEmitter = new EventEmitter();
     this.onDidRunCommandSuccessfullyEmitter = new EventEmitter();
 
+    await this.client.init()
     Context.subscriptions.push(
       this.client.start(),
       this.onWillRunCommandEmitter.event(this.onWillRunCommandHandler),
@@ -132,9 +133,9 @@ export class SQLToolsExtension implements IExtension {
       if (body) {
         body = encodeURIComponent(
           body
-          .replace(/---([^\-]+---\n\n)/gim, '')
-          .replace(/(- OS).+/gi, `$1: ${process.platform}, ${process.arch}`)
-          .replace(/(- Version).+/gi, `$1: v${VERSION}`));
+            .replace(/---([^\-]+---\n\n)/gim, '')
+            .replace(/(- OS).+/gi, `$1: ${process.platform}, ${process.arch}`)
+            .replace(/(- Version).+/gi, `$1: v${VERSION}`));
       }
 
       if (!template && !body) {
@@ -174,7 +175,7 @@ export class SQLToolsExtension implements IExtension {
       const supportProject = 'Support This Project';
       const releaseNotes = 'Release Notes';
       const message = `${DISPLAY_NAME} updated! Check out the release notes for more information.`;
-      const options = [ moreInfo, supportProject, releaseNotes ];
+      const options = [moreInfo, supportProject, releaseNotes];
       const res: string = await window.showInformationMessage(message, ...options);
       telemetry.registerMessage('info', message, res);
       switch (res) {

packages/log/src/index.ts
@@ -2,7 +2,7 @@ import factory from './lib/factory';
 /**
  * Export logger based on environment.
  *
- * When inside of VSCode, use extension output channel,
+ * When inside of VS Code, use extension output channel,
  * Otherwise write to console.log/stdout.
  *
  * logs when in LS: language server output channel

packages/log/src/lib/vscode.ts
@@ -1,7 +1,7 @@
-if (process.env.PRODUCT !== 'ext') { throw 'Cant use outputchannels outside of VSCode context'; }
+if (process.env.PRODUCT !== 'ext') { throw 'Cant use outputchannels outside of VS Code context'; }
 
-import { Writable }  from 'stream';
-import { window }  from 'vscode';
+import { Writable } from 'stream';
+import { window } from 'vscode';
 import factory from './factory';
 
 const outputChannel = window.createOutputChannel(process.env.DISPLAY_NAME || 'SQLTools');

packages/plugins/connection-manager/dependency-manager/extension.ts
@@ -1,4 +1,4 @@
-import { window as Win, window, ProgressLocation, commands } from 'vscode';
+import { window as Win, window, ProgressLocation, commands, env } from 'vscode';
 import { openExternal } from '@sqltools/vscode/utils';
 import { EXT_NAMESPACE, DOCS_ROOT_URL, DISPLAY_NAME } from '@sqltools/util/constants';
 import { getConnectionId } from '@sqltools/util/connection';
@@ -7,6 +7,7 @@ import { IExtensionPlugin, ILanguageClient, IExtension, IConnection, NodeDepende
 import { MissingModuleNotification } from '@sqltools/base-driver/dist/lib/notification';
 import { DriverNotInstalledNotification } from '@sqltools/language-server/src/notifications';
 import { getDataPath } from '@sqltools/util/path';
+import getShellExitCommand from '@sqltools/vscode/utils/get-shell-exit-cmd';
 
 export default class DependencyManager implements IExtensionPlugin {
   public readonly name = 'Dependency Manager Plugin';
@@ -67,16 +68,16 @@ export default class DependencyManager implements IExtensionPlugin {
                 depNamesString.push(depStr);
                 if (dep.args) args.push(...dep.args);
               })
-              progress.report({ message: `Installing "${depNamesString.join(", ")}". Please wait till it finishes. Check the opened terminal for more info.` });
-              terminal.sendText(`${dependencyManagerSettings.packageManager} ${args.join(" ")} && exit 0`);
+              progress.report({ message: `Installing "${depNamesString.join(", ")}". Please wait until it finishes. Check the opened terminal for more info.` });
+
+              terminal.sendText(`${dependencyManagerSettings.packageManager} ${args.join(" ")} && ${getShellExitCommand()}`);
             });
             progress.report({ increment: 100, message: `Finished installing ${depNamesString.join(", ")}` });
           });
           this.installingDrivers = this.installingDrivers.filter(v => v !== conn.driver);
           const opt = conn.name ? [`Connect to ${conn.name}`] : [];
           const rr = conn.name && autoUpdateOrInstall ? opt[0] : await Win.showInformationMessage(
-            `"${dependenciesName}" installed!\n
-Go ahead and connect!`,
+            `"${dependenciesName}" installed. Go ahead and connect!`,
             ...opt
           );
           if (rr === opt[0]) {
@@ -95,7 +96,7 @@ Go ahead and connect!`,
 
   private driverNotInstalled = async ({ driverName }: { driverName: DatabaseDriver }) => {
     if (!driverName) return;
-    const options = ['Search VSCode Marketplace'];
+    const options = ['Search VS Code Marketplace'];
     try {
       const r = await Win.showInformationMessage(
         `Driver ${driverName} is not installed.`,

commit cce76e01092e7eedd19b9d7d320840e3bfd64dd6

Fix some flakiness caused by PR #957 (#967)
* Move more from constructor to init()
this.registerBaseNotifications() was being called before this.client had been instantiated

* vscode.env.shell sometimes returns empty string
When this happened a PowerShell node-detection shell would fail to close.
Probably a timing issue, as adding a retry loop seems to fix it.

packages/extension/src/language-client/client.ts
@@ -23,12 +23,6 @@ export class SQLToolsLanguageClient implements ILanguageClient {
 
   private avoidRestart = false;
   constructor() {
-    this.onNotification(ExitCalledNotification, () => {
-      this.avoidRestart = true;
-    });
-
-    this.registerBaseNotifications();
-
     Config.addOnUpdateHook(async ({ event }) => {
       if (event.affectsConfig('useNodeRuntime')) {
         const res = await window.showWarningMessage('Use node runtime setting change. You must reload window to take effect.', 'Reload now');
@@ -63,8 +57,14 @@ export class SQLToolsLanguageClient implements ILanguageClient {
         return defaultErrorHandler.closed();
       },
     };
+    
+    this.onNotification(ExitCalledNotification, () => {
+      this.avoidRestart = true;
+    });
 
+    this.registerBaseNotifications();
   }
+
   public start() {
     return this.client.start();
   }
@@ -83,6 +83,7 @@ export class SQLToolsLanguageClient implements ILanguageClient {
     await this.client.onReady();
     return this.client.sendNotification.apply(this.client, arguments);
   }
+  
   public onNotification: LanguageClient['onNotification'] = async function () {
     await this.client.onReady();
     return this.client.onNotification.apply(this.client, arguments);
@@ -100,6 +101,7 @@ export class SQLToolsLanguageClient implements ILanguageClient {
           runtime = runtimePath;
         }
       } else {
+        log.info('Detecting node path (if this stalls check Terminal view for the stuck session and kill it)...');
         const nodePath = await detectNodePath();
         if (nodePath) {
           const message = `Node runtime auto-detected. Using ${nodePath}.`;

packages/extension/src/language-client/detect-node-path.ts
@@ -9,11 +9,12 @@ const nodeRuntimeTmpFile = getDataPath(".node-runtime");
 const detectNodePath = async (): Promise<string | null> => {
   try {
     const terminal = window.createTerminal({ name: "detect node runtime" });
-    await new Promise<void>((resolve) => {
+    const shellExitCommand = await getShellExitCommand();
+    await new Promise<void>(async (resolve) => {
       window.onDidCloseTerminal((e => e.processId === terminal.processId && resolve()));
       const nodeCmd = `require("fs").writeFileSync("${nodeRuntimeTmpFile}", process.execPath)`;
       const nodeCmdWindows = nodeCmd.replace(/\\/g, '\\\\').replace(/\"/g, '\\"');
-      terminal.sendText(`node -e '${process.platform === 'win32' ? nodeCmdWindows : nodeCmd}' && ${getShellExitCommand()}`);
+      terminal.sendText(`node -e '${process.platform === 'win32' ? nodeCmdWindows : nodeCmd}' && ${shellExitCommand}`);
     })
     return fs.readFileSync(nodeRuntimeTmpFile).toString();
   } catch (error) {

packages/vscode/utils/get-shell-exit-cmd.ts
@@ -1,7 +1,23 @@
 import { env } from 'vscode';
 
-export default function getShellExitCommand(code = 0) {
-  const isPowerShell = env.shell.match(/[\\/]pwsh(\.exe)?$/g);
+export default async function getShellExitCommand(code = 0) {
+
+  // env.shell in 1.71 sometimes returns '' unexpectedly.
+  // Use a retry loop to try and overcome this.
+  let shell = '';
+  for (let attempt = 1; attempt <= 10; attempt++) {
+    shell = env.shell;
+    if (shell !== '') {
+      break;
+    }
+    await new Promise(c => setTimeout(c, 500));
+  }
+
+  if (shell === '') {
+    throw new Error('No env.shell retrieved despite retries');
+  }
+
+  const isPowerShell = shell.match(/[\\/]pwsh(\.exe)?$/g);
   if (isPowerShell) return `$(exit ${code})`;
   return `exit ${code}`;
 }