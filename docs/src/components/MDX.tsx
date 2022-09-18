97892edb1c62f75c35e92e6024fd63393bde5357
Matheus Teixeira <me@mteixeira.dev>
2022-09-18 18:31:04 -0300

    [docs-v2] make new docs official

--- docs/src/components/MDX.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { components } from 'docz-theme-default';
-import MDXBase  from '@mdx-js/runtime'; //ts-diable-line
-
-const MDX = (props: any) => <MDXBase components={components}>{props.children}</MDXBase>
-
-export default MDX;
