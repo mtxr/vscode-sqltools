2068ce994b0627544fb19c54f8e1b1f66f29e8d4
gjsjohnmurray <johnm@georgejames.com>
2022-11-11 11:37:53 +0000

    Run yarn during postCreate step

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo '*** Please do not respond to any VS Code notification about opening a workspace until after this script completes ***'
+sleep 5
+
 # Suppress warnings triggered by engine value package.json of a VS Code extension
 yarn config set ignore-engines true -g
 
@@ -10,3 +13,6 @@ nvm install 14
 # Appending this to .bashrc means shells opened within the project tree will use the Node.js version specified in .nvmrc in the project root,
 # provided no overriding .nvmrc is found between the shell's cwd and that root folder.
 echo nvm use >> $HOME/.bashrc
+
+# Initialize for building
+yarn
