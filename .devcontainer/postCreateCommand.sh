2bf5bf466a03d96f57cff06b9f88b8cd935c33e9
gjsjohnmurray <johnm@georgejames.com>
2022-11-11 11:11:51 +0000

    Try to ensure use of node 14 when building

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# Suppress warnings triggered by engine value package.json of a VS Code extension
+yarn config set ignore-engines true -g
+
+# We have to build with Node.js version 14
 . $NVM_DIR/nvm.sh
 nvm install 14
+
+# Appending this to .bashrc means shells opened within the project tree will use the Node.js version specified in .nvmrc in the project root,
+# provided no overriding .nvmrc is found between the shell's cwd and that root folder.
+echo nvm use >> $HOME/.bashrc
