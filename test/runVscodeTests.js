12c3b7a2e3b6ac911ab7aaee46668b9e6336818c
John Murray <johnm@georgejames.com>
2023-06-21 22:42:25 +0100

    Update runVscodeTests.js to pin to 1.77.3

--- test/runVscodeTests.js
+++ test/runVscodeTests.js
@@ -3,11 +3,13 @@ const { extensionDevelopmentPath, testResultsFile, extensionTestsPath } = requir
 const printResults = require("./utils/printResults");
 
 async function main() {
+  // Testing is currently pinned to using 1.77.3 (see https://github.com/microsoft/vscode/issues/152938#issuecomment-1534782052)
+  const VSCODE_VERSION = "1.77.3";
   try {
     // Download VS Code, unzip it and run the integration test
     console.log("Start running tests. Will wait til get the results");
     const interval = setInterval(() => process.stderr.write("."), 1000);
-    const code = await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ['--disable-extensions'] });
+    const code = await runTests({ version: VSCODE_VERSION, extensionDevelopmentPath, extensionTestsPath, launchArgs: ['--disable-extensions'] });
     clearInterval(interval);
     const results = require(testResultsFile);
 
