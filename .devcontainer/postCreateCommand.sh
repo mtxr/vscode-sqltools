700898267ec37b2cd20c960c379678be680d3e57
gjsjohnmurray <johnm@georgejames.com>
2024-04-12 21:52:23 +0000

    Use node v16

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -11,9 +11,9 @@ sleep 5
 # Suppress warnings triggered by engine value package.json of a VS Code extension
 yarn config set ignore-engines true -g
 
-# We have to build with Node.js version 14
+# Build with Node.js version 16
 . $NVM_DIR/nvm.sh
-nvm install 14
+nvm install 16
 
 # Appending this to .bashrc means shells opened within the project tree will use the Node.js version specified in .nvmrc in the project root,
 # provided no overriding .nvmrc is found between the shell's cwd and that root folder.
