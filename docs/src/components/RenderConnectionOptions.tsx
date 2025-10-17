396880d9dd03be2ee596d305d5c58418ec7de19d
HyperAgent <196793856+sowhat1989@users.noreply.github.com>
2025-10-17 10:06:00 +0700

    Delete docs directory

--- docs/src/components/RenderConnectionOptions.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import RenderSettings from './RenderSettings'
-
-const RenderConnectionOptions = ({ exclude = [], include = [] }) => (
-  <RenderSettings path="['sqltools.connections'].items.properties" exclude={exclude} include={include} disableSearch title="Connection Options" />
-)
-
-export default RenderConnectionOptions;
