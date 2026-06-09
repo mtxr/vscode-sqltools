08089fc740a0fd545e43735847368725cbe2082c
gjsjohnmurray <johnm@georgejames.com>
2026-06-09 12:03:52 +0100

    Try a different approach

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -11,13 +11,10 @@ sleep 5
 # Avoid unactionable prompt during GitHub Codespace setup, which causes the setup to fail.
 #  ! Corepack is about to download https://registry.yarnpkg.com/yarn/-/yarn-1.22.22.tgz
 #  ? Do you want to continue? [Y/n]  
-npm install -g --no-update-notifier corepack@latest
-corepack install
-corepack enable yarn
-echo "yarn version $(yarn --version)"
-
-# Suppress warnings triggered by engine value package.json of a VS Code extension
-yarn config set ignore-engines true -g
+##npm install -g --no-update-notifier corepack@latest
+##corepack install
+##corepack enable yarn
+##echo "yarn version $(yarn --version)"
 
 # Build with Node.js version 20
 . $NVM_DIR/nvm.sh
@@ -26,6 +23,13 @@ nvm install 20
 # Appending this to .bashrc means shells opened within the project tree will use the Node.js version specified in .nvmrc in the project root,
 # provided no overriding .nvmrc is found between the shell's cwd and that root folder.
 echo nvm use >> $HOME/.bashrc
+
+# Install yarn
+npm install --global yarn
+echo "yarn version $(yarn --version)"
+
+# Suppress warnings triggered by engine value package.json of a VS Code extension
+yarn config set ignore-engines true -g
 
 # Initialize for building
 yarn
