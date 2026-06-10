e6ad1b0ec0c9d1824d05a68285871d3bfe697997
John Murray <johnm@georgejames.com>
2026-06-10 10:20:23 +0100

    createSshTunnel: support `passphrase` for private key (types) (#1604)
    * createSshTunnel: support `passphrase` for private key
    
    * Actual change
    
    * Add missing piece

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -261,6 +261,14 @@ export interface IConnection<DriverOptions = any> {
      * @memberof IConnection.sshOptions
      */
     privateKeyPath?: string;
+
+    /**
+     * Passphrase for the private key
+     * @type {string}
+     * @default null
+     * @memberof IConnection.sshOptions
+     */
+    passphrase?: string;
   };
 
   // WONT BE INCLUDED IN SETTINGS
@@ -341,6 +349,7 @@ export interface IConnectionDriver {
       username: string;
       password?: string;
       privateKeyPath?: string;
+      passphrase?: string;
     },
     db: {
       host: string;
